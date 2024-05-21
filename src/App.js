3cc6020b1d2bc2018fc63ab88059ebf27cf3da23
unknown <pedro@tecnocarcuritiba.com.br>
2024-05-21 17:00:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
 function App() {
+  const [coordenadas, setCoordenadas] = React.useState([])
+  const clickHandler = (e) => {
+    let X = e.clientX
+    let Y = e.clientY
+    setCoordenadas([...coordenadas, {x:X,y:Y}])
+  }
+
+
+  let positions = coordenadas.map(item => {
+    return(
+        <div key={item.key} style={{position:"absolute",left:`${item.x}px`,top:`${item.y}px`,background:"white",height:"5px",width:"5px"}}></div>
+    )
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={clickHandler} className="app">
+      {positions}
     </div>
   );
 }
