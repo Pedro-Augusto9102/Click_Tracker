5ef13c813fbaa278868722dd1a0b038f880de3ec
unknown <pedro@tecnocarcuritiba.com.br>
2024-05-23 14:02:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,15 @@ function App() {
     let Y = e.clientY
     setCoordenadas([...coordenadas, {x:X,y:Y}])
   }
-
-
   let positions = coordenadas.map(item => {
     return(
         <div key={item.key} style={{position:"absolute",left:`${item.x}px`,top:`${item.y}px`,background:"white",height:"5px",width:"5px"}}></div>
     )
   })
-
-
   return (
     <div onClick={clickHandler} className="app">
       {positions}
     </div>
   );
 }
-
 export default App;
