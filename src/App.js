96bdf0f61aa8af30191eb41a09ac90e302e4b688
Pedro <pedro@tecnocarcuritiba.com.br>
2024-06-11 20:43:11 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react"
+import React from "react";
 function App() {
-  const [coordenadas, setCoordenadas] = React.useState([])
+  const [coordenadas, setCoordenadas] = React.useState([]);
   const clickHandler = (e) => {
-    let X = e.clientX
-    let Y = e.clientY
-    setCoordenadas([...coordenadas, {x:X,y:Y}])
-  }
-  let positions = coordenadas.map(item => {
-    return(
-        <div key={item.key} style={{position:"absolute",left:`${item.x}px`,top:`${item.y}px`,background:"white",height:"5px",width:"5px"}}></div>
-    )
-  })
+    let X = e.clientX;
+    let Y = e.clientY;
+    setCoordenadas([...coordenadas, { x: X, y: Y }]);
+  };
+  let positions = coordenadas.map((item) => {
+    return (
+      <div
+        key={item.key}
+        style={{
+          position: "absolut",
+          left: `${item.x}px`,
+          top: `${item.y}px`,
+          background: "white",
+          height: "5px",
+          width: "5px",
+        }}
+      ></div>
+    );
+  });
   return (
     <div onClick={clickHandler} className="app">
       {positions}
